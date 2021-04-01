8d50573ff2aaf1bcec6155e60a9fdadf85d0cbec
Daniel Yatali <danielyatali902@gmail.com>
2021-04-01 04:38:38 +0000

    Made it look pretty I think kix

---
+++ script.js
@@ -0,0 +1,10 @@
+function change_color(event){
+  document.querySelector(".greet").style.textShadow = "2px 2px 0px purple";
+}
+function revert_color(event){
+  //document.querySelector(".greet").style.color = "gold";
+  document.querySelector(".greet").style.textShadow = "2px 2px 0px white";
+}
+document.querySelector("#browse").addEventListener("mouseover", change_color);
+
+document.querySelector("#browse").addEventListener("mouseout", revert_color);
